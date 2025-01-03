36afa50a4bce7c5542722ba1824cc7b8a3b948bf
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-01-03 19:38:06 +0100

    lint

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
       renderer:
         process.env.NODE_ENV === 'test'
           ? // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
-          undefined
+            undefined
           : {},
     }),
   ],
