d938d00172744aa00da07ca4092fd42b491e8adc
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-01-06 17:14:38 +0000

    fix ssh2 build

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,9 @@ export default defineConfig({
         vite: {
           build: {
             outDir: 'dist',
+            rollupOptions: {
+              external: ['ssh2'],
+            },
           },
         },
       },
