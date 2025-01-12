f9c4ccfd092059b62d6230d38aa2a2ab4976793e
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-01-12 20:23:11 +0100

    sign mac

---
+++ scripts/notarize.js
@@ -0,0 +1,21 @@
+import { notarize } from 'electron-notarize';
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+export default async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    tool: 'notarytool',
+    teamId: process.env.APPLE_TEAM_ID,
+    appBundleId: 'com.tweakphp.app',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+  });
+}
