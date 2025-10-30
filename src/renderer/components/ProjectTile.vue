cfb9f5509627f9bb0e6afba2eb3a041a570e37ce
Chung <nguyentranchung52th@gmail.com>
2025-10-30 21:44:23 +0900

    style(ProjectTile): adjust styling classes for better consistency
    Refine class names and structure to improve layout consistency between expanded and collapsed states. Remove redundant classes and ensure proper text alignment.

--- src/renderer/components/ProjectTile.vue
+++ src/renderer/components/ProjectTile.vue
@@ -30,13 +30,13 @@
     :class="{
       'bg-gradient-to-r from-primary-500 to-primary-800 text-white shadow-md': props.active,
       'border': !props.active,
-      'w-8 justify-center': !props.expanded,
-      'w-full justify-start px-3': props.expanded,
+      'w-8 justify-center capitalize': !props.expanded,
+      'w-full px-3': props.expanded,
     }"
-    class="text-md flex h-8 items-center rounded-lg capitalize cursor-pointer hover:shadow-md transition-all"
+    class="text-md flex h-8 rounded-lg cursor-pointer hover:shadow-md transition-all"
     v-tippy="{ content: props.tooltip, placement: props.tooltipPlacement }"
   >
-    <span class="flex items-center" :class="{ 'min-w-0 w-full': props.expanded }">
+    <span class="flex items-center" :class="{ 'w-full text-start': props.expanded }">
       <slot></slot>
     </span>
   </div>
