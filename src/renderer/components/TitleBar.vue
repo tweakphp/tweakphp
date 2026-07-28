d90addac12076a05c1cd3bfd5034098f59cedcca
David Galet <davidgalet@gmail.com>
2026-07-28 18:28:07 +0200

    fix: increase z-index of TitleBar for improved visibility

--- src/renderer/components/TitleBar.vue
+++ src/renderer/components/TitleBar.vue
@@ -92,7 +92,7 @@
 <template>
   <div
     id="title-bar"
-    class="fixed top-0 right-0 h-[38px] z-40 left-0 w-full border-b"
+    class="fixed top-0 right-0 h-[38px] z-50 left-0 w-full border-b"
     :style="{
       backgroundColor: settingsStore.colors.background,
       borderColor: settingsStore.colors.border,
